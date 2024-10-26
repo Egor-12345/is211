c2f8ef09df2ed501a7801d7f8c2cb5e3d1e4fd5f
Студент 10 Кабинет 409 <409-10@coopteh.local>
2024-10-26 14:27:03 +0700

    моя

--- tests/RouterTest.php
+++ tests/RouterTest.php
@@ -4,8 +4,20 @@ namespace Test;
 use PHPUnit\Framework\TestCase;
 use Routers\Router;
 
-class RouterTest extends TestCase {
+class ProductTest extends TestCase {
     public function test_router() {
+        $router = new Router();
+        $html = $router->route( "http://localhost/products" );
+        $pos= mb_strpos($html, "Добавить в корзину");
+        $this->assertNotEquals(false, $pos);
+    }
+    public function test_router1() {
+        $router = new Router();
+        $html = $router->route( "http://localhost" );
+        $pos= mb_strpos($html, "Приглашаем в наше онлайн-кафе");
+        $this->assertNotEquals(false, $pos);
+    }
+    public function test_router2() {
         $router = new Router();
         $html = $router->route( "http://localhost/orders" );
         $pos= mb_strpos($html, "Создание заказа");
